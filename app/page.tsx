e36799565364e8ac637f93bad8a8972e1fcd20b6
Lautaro Bravoj <lautarobravoj@gmail.com>
2025-03-18 11:35:49 +0100

    Actualización

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,24 @@ export default function Home() {
   return (
     <div style={{ textAlign: 'center', padding: '20px' }}>
       <Image src="/perfil.jpg" alt="Mi Foto" width={150} height={150} style={{ borderRadius: '50%' }} />
-      <h1>Tu Nombre</h1>
-      <h2>Tu Negocio</h2>
+      <h1>Lautaro Bravo</h1>
+      <h2>Programación</h2>
       <p>Breve descripción de tu trabajo</p>
 
       <section style={{ textAlign: 'center', marginTop: '20px' }}>
         <h2>Servicios</h2>
         <ul>
-          <li>🔧 Reparaciones</li>
-          <li>🏗 Remodelaciones</li>
-          <li>🏠 hola</li>
+          <li>🔧Diseño de Webs</li>
+          <li>🏗 tarjetas digitales</li>
+          <li>🏠 Manejo de redes</li>
         </ul>
       </section>
 
       <section style={{ textAlign: 'center', marginTop: '20px' }}>
         <h2>Contacto</h2>
         <a href="tel:+34624113764">📞 Llamar</a> | 
-        <a href="https://wa.me/34695541370">💬 WhatsApp</a> | 
-        <a href="https://maps.google.com/?q=Ubicacion">📍 Ubicación</a>
+        <a href="https://wa.me/34624113764">💬 WhatsApp</a> | 
+        <a href="https://maps.app.goo.gl/DUimcP6JJFMsxZnG6">📍 Ubicación</a>
       </section>
 
       <footer style={{ textAlign: 'center', padding: '20px' }}>
